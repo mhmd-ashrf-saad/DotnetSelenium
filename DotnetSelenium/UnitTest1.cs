307bfd957b76c8fcdb9fd6c9b1a5c66d68469a82
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-08-02 18:06:04 +0300

    writing First Selenium Test code in C#

--- DotnetSelenium/UnitTest1.cs
+++ DotnetSelenium/UnitTest1.cs
@@ -1,3 +1,6 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace DotnetSelenium
 {
     public class Tests
@@ -10,7 +13,19 @@ namespace DotnetSelenium
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //sudo code for setting up selenium
+            //1. Create a new instance of Selenium Web Driver
+            IWebDriver driver = new ChromeDriver();
+            //2. Navigate to the URL
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            //2a. Maximize the browser window
+            driver.Manage().Window.Maximize();
+            //3. Find the element
+            IWebElement webElement = driver.FindElement(By.Name("q"));
+            //4. Type in the element
+            webElement.SendKeys("Selenium");
+            //5. Click on the element
+            webElement.SendKeys(Keys.Return);
         }
     }
 }
