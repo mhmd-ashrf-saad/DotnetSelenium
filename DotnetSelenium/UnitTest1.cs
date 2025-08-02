c19420ec4a968098618ebaa87c5f84b06446b2c7
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-08-02 21:29:15 +0300

    added ShouldLoginToEAppWebsiteSuccessfully

--- DotnetSelenium/UnitTest1.cs
+++ DotnetSelenium/UnitTest1.cs
@@ -1,5 +1,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V136.Input;
+using OpenQA.Selenium.Support.UI;
 
 namespace DotnetSelenium
 {
@@ -11,7 +13,7 @@ namespace DotnetSelenium
         }
 
         [Test]
-        public void Test1()
+        public void Should_Open_Browser_And_Search_Selenium()
         {
             //sudo code for setting up selenium
             //1. Create a new instance of Selenium Web Driver
@@ -27,5 +29,34 @@ namespace DotnetSelenium
             //5. Click on the element
             webElement.SendKeys(Keys.Return);
         }
+
+        [Test]
+        public void ShouldLoginToEAppWebsiteSuccessfully()
+        {
+            // Setup: Create a new instance of ChromeDriver and configure waits
+            using IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Manage().Window.Maximize();
+
+            // Navigate to the EA App website
+            driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+
+            // Click the login link
+            wait.Until(d => d.FindElement(By.Id("loginLink"))).Click();
+
+            // Enter username
+            wait.Until(d => d.FindElement(By.Name("UserName"))).SendKeys("admin");
+
+            // Enter password
+            driver.FindElement(By.Name("Password")).SendKeys("password");
+
+            // Click login button
+            driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+
+            // Assert: Verify login was successful by checking for the logout link
+            IWebElement logoutLink = wait.Until(d => d.FindElement(By.LinkText("Log off")));
+            Assert.IsTrue(logoutLink.Displayed, "Login was not successful - logout link not found.");
+        }
+
     }
 }
